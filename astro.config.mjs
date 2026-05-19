206d43530694e8db8ad8e17a704dca926c77648d
Daisy <116069474+DaisyC-YT@users.noreply.github.com>
2026-05-19 15:03:45 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://github.com/DaisyC-YT/autoseo",
+  site: 'https://DaisyC-YT.github.io',
+  base: '/autoseo',
+  output: 'static',
 });
